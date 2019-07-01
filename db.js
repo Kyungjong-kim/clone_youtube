8398b1835ab8f3434805f98388e54fc86417f1b0
Kimkungjong <rudwhd4474@gmail.com>
2019-07-01 16:37:46 +0900

    build mongoDB

--- db.js
+++ db.js
@@ -1,34 +1,17 @@
-export const videos = [{
-    id: 321231,
-    title: 'Video',
-    description: 'this is someting',
-    views: 24,
-    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-        id: 12111,
-        name: "Kim",
-        email: "rudwhd4474@gmail.com"
-    }
-}, {
-    id: 321231,
-    title: 'Video 1',
-    description: 'this someting',
-    views: 24,
-    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-        id: 12111,
-        name: "Kim",
-        email: "rudwhd4474@gmail.com"
-    }
-}, {
-    id: 321231,
-    title: 'Video 2',
-    description: 'this is someting',
-    views: 24,
-    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-        id: 12111,
-        name: "Kim",
-        email: "rudwhd4474@gmail.com"
-    }
-}, ]
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("Connected to DB");
+const handleError = () => console.log(`Error on DB connection : ${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
